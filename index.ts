06727e305d07aa1c2ba5feab346db0c3ea19221c
Jens <25007405+JensdeVlaming@users.noreply.github.com>
2024-02-21 16:09:51 +0100

    update index file

--- index.ts
+++ index.ts
@@ -5,14 +5,12 @@ import { ConsoleNotification } from './src/Notifications/ConsoleNotification'
 import EmailNotification from './src/Notifications/EmailNotification'
 import Order from './src/Order'
 
-const consoleNotification = new ConsoleNotification()
-
 const order = new Order(1, false)
 const movie = new Movie('Mr. Bean')
 const movieScreening = new MovieScreening(new Date('2024-01-31T15:01:10.204Z'), 20, movie)
 const ticket = new MovieTicket(movieScreening, false, 1, 2)
 
-order.attach(consoleNotification)
+order.attach(new ConsoleNotification())
 
 order.addSeatToReservation(ticket)
 order.addSeatToReservation(ticket)
