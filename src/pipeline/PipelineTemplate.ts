4f6c6366f441213dd51f968b4af4d083eb002074
Zuijd <dca.vanzuijdam@student.avans.nl>
2024-03-18 20:34:45 +0100

    Added new concrete pipelines

--- src/pipeline/PipelineTemplate.ts
+++ src/pipeline/PipelineTemplate.ts
@@ -1,5 +1,11 @@
 // Template for Template Pattern
 export default abstract class PipelineTemplate {
+	protected settings: { [key: string]: any }
+
+	constructor(settings?: { [key: string]: any }) {
+		this.settings = settings || {}
+	}
+
 	async executeAsync(): Promise<void> {
 		await this.fetchSourceAsync()
 		await this.installPackagesAsync()
@@ -7,6 +13,7 @@ export default abstract class PipelineTemplate {
 		await this.testAsync()
 		await this.analyzeAsync()
 		await this.deployAsync()
+		await this.optionalStepsAsync()
 		await this.executeUtilityAsync()
 		await this.finishAsync()
 	}
@@ -15,15 +22,31 @@ export default abstract class PipelineTemplate {
 		console.log('Fetching source...')
 	}
 
+	protected async installPackagesAsync(): Promise<void> {
+		console.log('Installing packages...')
+	}
+
+	protected async buildAsync(): Promise<void> {
+		console.log('Building...')
+	}
+
+	protected async testAsync(): Promise<void> {
+		console.log('Testing...')
+	}
+
+	protected async analyzeAsync(): Promise<void> {
+		console.log('Analyzing...')
+	}
+
+	protected async executeUtilityAsync(): Promise<void> {
+		console.log('Executing utility...')
+	}
+
 	protected async finishAsync(): Promise<void> {
 		console.log('Finishing...')
 		console.log('Pipeline executed successfully!')
 	}
 
-	protected abstract installPackagesAsync(): Promise<void>
-	protected abstract buildAsync(): Promise<void>
-	protected abstract testAsync(): Promise<void>
-	protected abstract analyzeAsync(): Promise<void>
 	protected abstract deployAsync(): Promise<void>
-	protected abstract executeUtilityAsync(): Promise<void>
+	protected abstract optionalStepsAsync(): Promise<void>
 }
